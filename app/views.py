3ff525097edceb338d7c5d8f226ef538e9084cd4
juntanaka <edamame.zombie@gmail.com>
2019-11-19 02:07:44 +0900

    https://djangobrothers.com/tutorials/memo_app/display_memos/ ここから

--- app/views.py
+++ app/views.py
@@ -1,5 +1,18 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+
+from .models import Memo
 
 
 def index(request):
-    return render(request, 'app/index.html')
+    memos = Memo.objects.all().order_by('-updated_datetime')
+    return render(request, 'app/index.html', {'memos': memos})
+
+
+def detail(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id)
+    return render(request, 'app/detail.html', {'memo': memo})
+
+
+def new_memo(request):
+    return render(request, 'app/new_memo.html')
