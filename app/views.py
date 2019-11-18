bf93d2f9d9e51c3ba47abbcab366709dc400e2c9
juntanaka <edamame.zombie@gmail.com>
2019-11-19 04:08:16 +0900

    やっとできた。死ぬ

--- app/views.py
+++ app/views.py
@@ -24,3 +24,25 @@ def new_memo(request):
     else:
         form = MemoForm
     return render(request, 'app/new_memo.html', {'form': form})
+
+
+from django.views.decorators.http import require_POST
+
+
+@require_POST
+def delete_memo(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id)
+    memo.delete()
+    return redirect('app:index')
+
+
+def edit_memo(request, memo_id):
+    memo = get_object_or_404(Memo, id=memo_id)
+    if request.method == "POST":
+        form = MemoForm(request.POST, instance=memo)
+        if form.is_valid():
+            form.save()
+            return redirect('app:index')
+    else:
+        form = MemoForm(instance=memo)
+    return render(request, 'app/edit_memo.html', {'form': form, 'memo': memo})
